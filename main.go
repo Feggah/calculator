03fc9041331f820c68dc342caefd6202f2bfcd03
Feggah <gabidferreira9@gmail.com>
2022-04-12 19:23:17 -0300

    Add Icon when Application is running
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"github.com/Feggah/calculator/calculator"
+	"github.com/Feggah/calculator/utils"
 )
 
 func main() {
 	app := app.New()
+	app.SetIcon(utils.ResourceIconPng)
 
 	c := calculator.NewCalculator()
 	c.LoadUserInterface(app)
